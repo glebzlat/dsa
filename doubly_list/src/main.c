1bdfedbb36a0b42432885b1629169837c95631f6
glebzlat <mrkotofey@inbox.ru>
2024-09-14 18:01:07 +0500

    Use test suite

--- doubly_list/src/main.c
+++ doubly_list/src/main.c
@@ -1,5 +1,13 @@
 #include "dlist.h"
+#include <test_suite.h>
 
-int main(void) {
-  return 0;
+int main(int argc, char** argv) {
+  test_suite* suite = test_suite_init(argc, argv);
+
+  {
+    dlist* lst = dlist_init();
+    dlist_deinit(lst);
+  }
+
+  return test_suite_finish(suite);
 }
